2cb9689a4ad256ad6c4dea196259fde2f4a91322
rhaberme <rhaberme@rhrk.uni-kl.de>
2022-12-11 15:41:01 +0100

    latest updates

--- Start.py
+++ Start.py
@@ -37,7 +37,7 @@ st.markdown("""
 }
 </style>""", unsafe_allow_html=True)
 
-st.title("Start")
+st.title("Sessions")
 
 # from streamlit_option_menu import option_menu
 
@@ -50,6 +50,11 @@ st.title("Start")
 def init_connection():
     return sqlite3.connect("database/durak_db")
 
+st.markdown("### Aktuelles Spiel:")
+
+winner_looser_placeholder = st.empty()
+player_ingame_placeholder= st.empty()
+col5, col6, col7 = st.columns(3)
 
 is_open, session_id, start_time, player_ingame = d_c.check_session_open()
 
@@ -64,63 +69,65 @@ if is_open:
 
     game_number, last_winner, last_looser = d_c.return_last_game_results(session_id)
 
-    st.markdown("### Aktuelles Spiel:")
 
     current_players_string = ""
     for ingame_player in current_players:
         current_players_string+= f"""{ingame_player} - """
     current_players_string = current_players_string.rstrip(" -")
-    st.metric("Mitspieler", current_players_string)
-    col5, col6, col7 = st.columns([0.3, 0.3, 1])
+    player_ingame_placeholder.metric("Mitspieler", current_players_string)
     col5.metric("Spiel-count", game_number)
     col6.metric("Letzter Gewinner", last_winner)
     col7.metric("Letzter Verlierer", last_looser)
-    col3, col4 = st.columns([0.3, 1])
 
-
+    col3, col4 = winner_looser_placeholder.columns(2)
     winner = col3.selectbox("Gewinner", current_players, index=0)
     looser = col4.selectbox("Verlierer", current_players, index=1)
-    save_btn = st.button("Speichern")
+    save_btn = col3.button("Speichern")
     if save_btn:
         d_c.insert_new_game_results(session_id, winner=winner, looser=looser)
         d_c.update_player_scores(current_players, winner, looser)
+        st.experimental_rerun()
         st.success("Gespeichert")
 
-new_session_exp = st.expander("Neue Session")
-player_names = list(d_c.return_players_table().names)
-player_in_game = new_session_exp.multiselect("Mitspieler auswählen", player_names)
 
-if len(player_in_game) > 0:
-    iter_nr = 1
-    new_session_exp.markdown("#### Reihenfolge:")
-    for player in player_in_game:
-        new_session_exp.write(f"Spieler {iter_nr}: {player}")
-        iter_nr += 1
+    end_session_btn = st.button("Session beenden")
 
-start_session_btn = new_session_exp.button("Starten")
-col1, col2 = st.columns([0.3, 1])
-end_session_btn = col1.button("Session beenden")
-get_data_btn = col2.button("Sessions anzeigen")
-refresh_btn = st.button("Aktualisieren")
+    if end_session_btn:
+        if is_open:
+            d_c.end_session(session_id)
+            st.success("Session beendet")
+            is_open, session_id, start_time, current_players = d_c.check_session_open()
+            st.experimental_rerun()
 
+        else:
+            st.success("Keine Session offen")
+else:
 
-if start_session_btn:
-    if not is_open:
-        session_id = d_c.start_new_session(player_in_game)
-        d_c.create_session_table(session_id)
-        new_session_exp.success(f"Neue Session gestartet")
-        is_open, session_id, start_time, current_players = d_c.check_session_open()
-    else:
-        st.success(f"Session Nr. {session_id} noch offen. Beginn der Session: {d_p.unixtime_to_dt64(start_time)}")
+    # new_session_exp = st.expander("Neue Session")
+    player_names = list(d_c.return_players_table().names)
+    player_in_game = st.multiselect("Mitspieler auswählen", player_names)
 
-if end_session_btn:
-    if is_open:
-        d_c.end_session(session_id)
-        st.success("Session beendet")
-        is_open, session_id, start_time, current_players = d_c.check_session_open()
-    else:
-        st.success("Keine Session offen")
+    if len(player_in_game) > 0:
+        iter_nr = 1
+        st.markdown("#### Reihenfolge:")
+        for player in player_in_game:
+            st.write(f"Spieler {iter_nr}: {player}")
+            iter_nr += 1
 
-if get_data_btn:
-    st.table(d_c.return_sessions_table())
+    start_session_btn = st.button("Starten")
+    if start_session_btn:
+        if not is_open:
+            session_id = d_c.start_new_session(player_in_game)
+            d_c.create_session_table(session_id)
+            st.success(f"Neue Session gestartet")
+            is_open, session_id, start_time, current_players = d_c.check_session_open()
+            st.experimental_rerun()
+
+        else:
+            st.success(f"Session Nr. {session_id} noch offen. Beginn der Session: {d_p.unixtime_to_dt64(start_time)}")
+
+#get_data_btn = st.button("Sessions anzeigen")
+
+#if get_data_btn:
+#    st.table(d_c.return_sessions_table())
 
